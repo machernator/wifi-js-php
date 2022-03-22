4e13905408ed3548078ddf6aab76cd072592f648
Thomas Macher <machernator@gmail.com>
2022-03-22 10:29:45 +0100

    Positionierung anhand input

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -4,5 +4,12 @@ let autocomplete = document.getElementById('autocomplete');
 
 // Event Listener zuweisen
 search.onclick = function (e) {
-	console.log('klick');
+	// this im Kontext event: das Element, dem das Event zugewiesen wurde
+	// Breite, Höhe, viewport Koordinaten auslesen
+	const bcr = this.getBoundingClientRect();
+	autocomplete.style.left = bcr.x + window.scrollX + 'px';
+	autocomplete.style.top = bcr.y + window.scrollY + bcr.height + 'px';
+	autocomplete.style.width = bcr.width + 'px';
+
+
 };
