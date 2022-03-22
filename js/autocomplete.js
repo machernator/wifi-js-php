41be2c53a1096251fdb1f79902d2c1f5df1366fe
Thomas Macher <machernator@gmail.com>
2022-03-22 10:12:55 +0100

    click event handler

---
+++ js/autocomplete.js
@@ -0,0 +1,8 @@
+// Elemente selektieren
+let search = document.getElementById('search');
+let autocomplete = document.getElementById('autocomplete');
+
+// Event Listener zuweisen
+search.onclick = function (e) {
+	console.log('klick');
+};
