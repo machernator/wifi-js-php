f467217fc4a03ee5ec8ed5c03e5b25fd1b5d18eb
Thomas Macher <machernator@gmail.com>
2022-03-22 11:08:36 +0100

    keyup event, show autocomplete

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -3,13 +3,17 @@ let search = document.getElementById('search');
 let autocomplete = document.getElementById('autocomplete');
 
 // Event Listener zuweisen
-search.onclick = function (e) {
-	// this im Kontext event: das Element, dem das Event zugewiesen wurde
-	// Breite, Höhe, viewport Koordinaten auslesen
-	const bcr = this.getBoundingClientRect();
-	autocomplete.style.left = bcr.x + window.scrollX + 'px';
-	autocomplete.style.top = bcr.y + window.scrollY + bcr.height + 'px';
-	autocomplete.style.width = bcr.width + 'px';
+search.onkeyup = function (e) {
+	// Falls nicht vorhanden, klasse show hinzufügen
+	if (!autocomplete.classList.contains('show')) {
+		autocomplete.classList.add('show');
+		// this im Kontext event: das Element, dem das Event zugewiesen wurde
+		// Breite, Höhe, viewport Koordinaten auslesen
+		const bcr = this.getBoundingClientRect();
+		autocomplete.style.left = bcr.x + window.scrollX + 'px';
+		autocomplete.style.top = bcr.y + window.scrollY + bcr.height + 'px';
+		autocomplete.style.width = bcr.width + 'px';
+	}
 
 
 };
